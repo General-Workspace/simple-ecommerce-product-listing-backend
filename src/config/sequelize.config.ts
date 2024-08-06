a992c91656f12d537efc372a359f2d70181bb292
aniebietafia <aniebietafia87@gmail.com>
2024-08-06 21:55:38 +0100

    feat: setting up sequelize database

--- src/config/sequelize.config.ts
+++ src/config/sequelize.config.ts
@@ -4,12 +4,6 @@ import { Product } from "../models/products.model";
 import dotenv from "dotenv";
 dotenv.config();
 
-console.log({
-  database: process.env["DB_NAME"] as string,
-  username: process.env["DB_USER"] as string,
-  password: process.env["DB_PASSWORD"] as string,
-});
-
 export const sequelize = new Sequelize({
   dialect: "postgres",
   database: process.env["DB_NAME"] as string,
