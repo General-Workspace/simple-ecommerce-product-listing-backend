bca276af9e137ca552c3dc4f8d8e31ab953f9069
aniebietafia <aniebietafia87@gmail.com>
2024-08-06 21:53:49 +0100

    feat: setting up sequelize database

---
+++ src/config/sequelize.config.ts
@@ -0,0 +1,19 @@
+import { Sequelize } from "sequelize-typescript";
+import { User } from "../models/users.model";
+import { Product } from "../models/products.model";
+import dotenv from "dotenv";
+dotenv.config();
+
+console.log({
+  database: process.env["DB_NAME"] as string,
+  username: process.env["DB_USER"] as string,
+  password: process.env["DB_PASSWORD"] as string,
+});
+
+export const sequelize = new Sequelize({
+  dialect: "postgres",
+  database: process.env["DB_NAME"] as string,
+  username: process.env["DB_USER"] as string,
+  password: process.env["DB_PASSWORD"] as string,
+  models: [User, Product],
+});
